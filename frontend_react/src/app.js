6b80b187ee1571f5826a732a2a4d54c63e1396d7
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2022-03-12 12:28:00 +0000

    added react

---
+++ frontend_react/src/app.js
@@ -0,0 +1,16 @@
+import react from 'react';
+
+ const App = () => {
+    return <h1>Hello World</h1>;
+
+}
+export default App;
+// import React from 'react'
+
+// const app = () => {
+//   return (
+//     <div>app</div>
+//   )
+// }
+
+// export default app
