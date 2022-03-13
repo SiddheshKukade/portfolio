055d8e6dafdd9846888c2d7b26090eef24a38b69
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2022-03-13 13:13:13 +0000

    👌 IMPROVE: navbar styles

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { HiMenuAlt4, Hix } from "react-icons/hi";
+import { motion } from "framer-motion";
 import "./Navbar.scss";
 import { images } from "../../constants";
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
@@ -15,6 +19,16 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onclick={() => setToggle(true)} />
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: "easeOut" }}
+          ></motion.div>
+        )}
+      </div>
     </nav>
   );
 };
