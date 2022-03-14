8371b84d33d7dc5cd156247d67551b42180a4c2d
SiddheshKukade <littlelord124@gmail.com>
2022-03-14 13:20:39 +0530

    📦 NEW: About done now need to do the sanity integration

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './About.scss'
+import { motion } from 'framer-motion'
+import { images } from '../../constants'
 const About = () => {
+  const abouts = [{ title: 'Web Developement', description: 'I am a good web developer.', imgUrl: images.about01 },
+  { title: 'Machine Learning', description: 'I am a good web designer.', imgUrl: images.about02 },
+  { title: 'UI / UX', description: 'I am a very good at UI/UX.', imgUrl: images.about03 },
+  { title: 'Web Animations', description: 'I am a good web developer.', imgUrl: images.about04 }
+  ]
   return (
-    <div>About</div>
+    <>
+      <h2 className='head-text'> I know that <span>Good Code</span> <br /> means<span> Good Business</span></h2>
+      <div className='app__profiles'>
+        {
+          abouts.map((about, index) => (<motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
+          </motion.div>))
+        }
+      </div>
+    </>
   )
 }
 
