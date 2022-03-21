011aba02c5849cd122e2119ff5ef327669f6726d
SiddheshKukade <littlelord124@gmail.com>
2022-03-21 20:03:10 +0530

    📦 NEW: More Images added for home screen

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -38,6 +38,8 @@ import spotify from '../assets/spotify.png';
 import logonew from '../assets/logonew.png'
 import profile1 from '../assets/profile1.png'
 import profile2 from '../assets/profile2.png'
+import profile3 from '../assets/profile3.png'
+import profile4 from '../assets/profile4.png'
 
 export default {
   email,
@@ -75,5 +77,7 @@ export default {
   spotify,
   logonew,
   profile1, 
-  profile2
+  profile2,
+  profile3,
+  profile4
 };
