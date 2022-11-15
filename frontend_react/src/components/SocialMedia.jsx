768f9d457abf7d38536762fa80c4094f47910fcb
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2022-11-15 14:16:24 +0000

    Add/title and Social Links

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
-import { BsTwitter, BsInstagram, BsGithub } from 'react-icons/bs'
-import {SiKaggle, SiGmail} from 'react-icons/si'
-import { FaFacebookF } from 'react-icons/fa'
+import React from "react";
+import { BsTwitter, BsInstagram, BsGithub } from "react-icons/bs";
+import { SiKaggle, SiGmail } from "react-icons/si";
+import { FaFacebookF } from "react-icons/fa";
+import ReactTooltip from "react-tooltip";
 const SocialMedia = () => {
-    return (
-        <div className='app__social'>
-            <div><a target="_blank" href='https://github.com/SiddheshKukade'><BsGithub /></a></div>
-            <div ><a target="_blank" href='https://instagram.com/SiddheshKukade'><BsInstagram /></a></div>
-            <div ><a><SiGmail /></a></div>
-        </div>
-    )
-}
+  return (
+    <div className="app__social">
+      <div title="Email: contact@siddheshkukade.com">
+        <a
+          target="_blank"
+          href="mailto:contact@siddheshkukade.com"
+          rel="noreferrer"
+        >
+          <SiGmail />
+        </a>
+      </div>
+      <div title="Check Out my Github Profile and Open Source Contributions">
+        <a
+          target="_blank"
+          href="https://github.com/SiddheshKukade"
+          rel="noreferrer"
+        >
+          <BsGithub />
+        </a>
+      </div>
+      <div title="Check Out my Instagram">
+        <a
+          target="_blank"
+          href="https://instagram.com/SiddheshKukade"
+          rel="noreferrer"
+        >
+          <BsInstagram />
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default SocialMedia
+export default SocialMedia;
